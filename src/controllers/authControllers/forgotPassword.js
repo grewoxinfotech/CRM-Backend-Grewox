83d7c1b58b906a21bf66884703e975baeeef5a1a
yashgrewox <yashgrewox@gmail.com>
2025-05-03 13:54:18 +0530

    refactor: update forgotPassword controller to improve email handling and error response
    This commit modifies the forgotPassword controller to utilize a new sendEmail function for sending password reset emails, enhancing email handling. Additionally, it improves error handling by ensuring that the response includes the error message only if it exists. The password validation message in resetPassword has also been updated for clarity.

--- src/controllers/authControllers/forgotPassword.js
+++ src/controllers/authControllers/forgotPassword.js
@@ -3,7 +3,7 @@ import User from "../../models/userModel.js";
 import validator from "../../utils/validator.js";
 import responseHandler from "../../utils/responseHandler.js";
 import { generateOTP } from "../../utils/otpService.js";
-import sgMail from "../../utils/emailService.js";
+import sgMail, { sendEmail } from "../../utils/emailService.js";
 import { getPasswordResetEmailTemplate } from "../../utils/emailTemplates.js";
 import { EMAIL_FROM, OTP_CONFIG } from "../../config/config.js";
 import jwt from "jsonwebtoken";
@@ -24,6 +24,14 @@ export default {
                 return responseHandler.notFound(res, "User not found");
             }
 
+            const clientMail = await User.findOne({ where: { id: user.client_id } });
+
+            const mail = clientMail.email;
+
+            if (!user && !mail) {
+                return responseHandler.notFound(res, "User not found");
+            }
+
             const otp = generateOTP(OTP_CONFIG.LENGTH);
             user.resetPasswordOTP = otp;
             user.resetPasswordOTPExpiry = Date.now() + OTP_CONFIG.EXPIRY.RESET_PASSWORD;
@@ -35,15 +43,16 @@ export default {
                 { expiresIn: '15m' }
             );
             const emailTemplate = getPasswordResetEmailTemplate(user.username, otp);
-            await sgMail.send({
-                to: email,
-                from: EMAIL_FROM,
-                subject: 'Password Reset Request',
-                html: emailTemplate
-            });
+            await sendEmail(mail, 'Password Reset Request', emailTemplate);
+            // await sgMail.send({
+            //     to: email,
+            //     from: EMAIL_FROM,
+            //     subject: 'Password Reset Request',
+            //     html: emailTemplate
+            // });
             return responseHandler.success(res, "Password reset OTP sent to your email", { sessionToken });
         } catch (error) {
-            return responseHandler.internalServerError(res, error.message);
+            return responseHandler.internalServerError(res, error?.message);
         }
     }
 }; 
--- src/controllers/authControllers/resetPassword.js
+++ src/controllers/authControllers/resetPassword.js
@@ -11,7 +11,7 @@ export default {
                 .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
                 .required()
                 .messages({
-                    'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+                    'string.pattern.base': 'create a strong password'
                 })
         })
     }),
