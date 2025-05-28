22fc360a8156b212bae0fd9544c43f8b97ee1b4c
Hacke0011 <palakardeshna01@gmail.com>
2025-05-28 12:44:32 +0530

    fix: correct import statement for getAllContacts in contactController

--- src/controllers/contactController/index.js
+++ src/controllers/contactController/index.js
@@ -2,7 +2,7 @@ import createContact from './createContact.js';
 import getContactById from './getContactbyId.js';
 import updateContact from './updateContact.js';
 import deleteContact from './deleteContact.js';
-import getAllContacts from './getAllcontacts.js';
+import getAllContacts from './getAllContacts.js';
 
 export {
     createContact,
